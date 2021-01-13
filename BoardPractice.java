ff0a3c1dd6835ffa26049c64ac084de8aa4b953b
Rienzi <rienziken.fernandez@kennedyhs.org>
2021-01-12 20:22:14 -0800

    update

--- BoardPractice.java
+++ BoardPractice.java
@@ -25,7 +25,7 @@ public class BoardPractice
     }
     static void printBoard(String[][] board) {
         
-        for (int i = 0; 1 < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++){
                 System.out.print("     " + board[i][j] + "     ");
             }
